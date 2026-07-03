45f11e7cdde219cb5f4d7093f1e37818b9370657
Hashem Znati <hashem.znati.04@gmail.com>
2026-07-03 16:07:37 +0100

    All tests for postcode_cli.py now pass.

--- postcode_cli.py
+++ postcode_cli.py
@@ -11,20 +11,21 @@ def parser_args():
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument('-m', '--mode', choices=['validate', 'complete'])
+    parser.add_argument(
+        '--mode', '-m', choices=['validate', 'complete'], required=True)
     parser.add_argument('postcode', type=str)
     args = parser.parse_args()
-
+    postcode = args.postcode.strip().upper()
     if args.mode == "validate":
-        val = validate_postcode(args.postcode)
+        val = validate_postcode(postcode)
         if val is True:
-            print(f"{args.postcode} is a valid postcode.")
+            print(f"{postcode} is a valid postcode.")
         else:
-            print(f"{args.postcode} is not a valid postcode.")
+            print(f"{postcode} is not a valid postcode.")
     elif args.mode == "complete":
-        codes = get_postcode_completions(args.postcode)
+        codes = get_postcode_completions(postcode)
         if codes is None or len(codes) == 0:
-            print(f"No matches for {args.postcode}.")
+            print(f"No matches for {postcode}.")
         else:
             counter = 0
             for i in range(len(codes)):
